9df82d10aee1190ef6f2e45a9314aa93f6de1f42
github username <github email address>
2019-12-07 12:10:50 -0500

    Adds styling to image cards

--- client/src/components/artCard.js
+++ client/src/components/artCard.js
@@ -2,21 +2,50 @@ import React from 'react';
 
 const ArtCard = props => {
 
-  const cardStyle = {
-    width: 'auto',
-    height: '300px',
-    padding: '20px',
-    margin: '10px 6px 6px 10px',
-    textDecoration: 'none',
-    background: 'grey',
+  const overlay = {
+    position: 'absolute',
+    top: '0',
+    bottom: '0',
+    left: '0',
+    right: '0',
+    height: '100%',
+    width: '100%',
+    opacity: '0',
+    transition: '.5s ease',
+    background_color: '#008CBA',
+    opacity: "1"
+  }
+
+  const container = {
+    position: 'relative',
+    width: '50%',
     float: 'left'
   }
 
+  const textOverlay = {
+    color: 'white',
+    font_size: '20px',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    webkit_transform: 'translate(-50%, -50%)',
+    ms_transform: 'translate(-50%, -50%)',
+    transform: 'translate(-50%, -50%)',
+    text_align: 'center',
+    opacity: '1'
+  }
+
+  const image = {
+    width: 'auto',
+    height: 'auto'
+  }
+
   const art = props.art.map((work, index) => {
-      return <div style={cardStyle} key={index}>
-        <img src={work._links.thumbnail.href}/>
-        <p>{work.title}, {work.collecting_institution}</p>
-      </div>;
+      return <div key={index} style={container}>
+        <img style={image} src={work._links.thumbnail.href}/>
+        <div style={overlay}>
+        <div style={textOverlay}>{work.title}, {work.collecting_institution}</div>
+        </div> </div>;
   })
 
   return (
